acd0f056805e42b32f2eb0c7c01b8ead15778fd2
BenMql <benjamin.miquel@tutanota.com>
2021-05-18 17:31:18 +0200

    issue #11 --- fixed plotting routine (missing z)

--- etc/python_scripts/plot_xyAvg_timeseries.py
+++ etc/python_scripts/plot_xyAvg_timeseries.py
@@ -5,7 +5,7 @@ from sys import argv
 plt.figure()
 time = np.fromfile('./Timeseries/time.dat', dtype=np.float_)
 for iarg in range(0,len(argv)-1,2):
-    avgQty = np.fromfile('./Timeseries/'+argv[iarg+1]+'_XYavg_'+str(int(argv[iarg+2])).zfill(5)+'.dat', dtype=np.float_)
+    avgQty = np.fromfile('./Timeseries/'+argv[iarg+1]+'_XYavg_z'+str(int(argv[iarg+2])).zfill(5)+'.dat', dtype=np.float_)
     plt.plot(time, avgQty [: time.shape[0]])
     del avgQty
 
